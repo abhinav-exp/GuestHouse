598833d5fe2a1df92d6f0caebeeda30580aca439
abhinav-exp <52093002+abhinav-exp@users.noreply.github.com>
2021-05-29 20:09:27 +0530

    Add files via upload

---
+++ GuestHouse.sh
@@ -0,0 +1,6 @@
+source vir/bin/activate
+django-admin startproject GuestHouse
+cd GuestHouse                       
+python3 manage.py startapp Staff    
+python3 manage.py startapp Organiser
+python3 manage.py startapp Public   
